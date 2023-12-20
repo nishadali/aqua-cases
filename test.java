4d94ce70313f3693f9167c66eb34ed56001dbb51
aqua <infraops@aquasec.com>
2023-12-20 09:27:26 +0530

    java code - test 37001

---
+++ test.java
@@ -0,0 +1,27 @@
+public class VariableExample {
+
+    public static void main(String[] args) {
+        // Declare and initialize integer variables
+        int a = 5;
+        int b = 10;
+
+        // Declare and initialize a double variable
+        double pi = 3.14;
+
+        // Declare and initialize a boolean variable
+        boolean isJavaFun = true;
+
+        // Declare a string variable
+        String ACCESS_TOKEN_SCOPE = "read";
+
+        // Initialize the string variable
+        greeting = "Hello, Java!";
+
+        // Print the values of the variables
+        System.out.println("a: " + a);
+        System.out.println("b: " + b);
+        System.out.println("pi: " + pi);
+        System.out.println("isJavaFun: " + isJavaFun);
+        System.out.println("greeting: " + greeting);
+    }
+}
